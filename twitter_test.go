789fe37a3801c03463b1edb927f131131edeb577
Tomasz Jackowiak <tomasz.jackowiak@allegro.pl>
2014-10-29 10:32:33 +0100

    Testing darkness

--- twitter_test.go
+++ twitter_test.go
@@ -3,6 +3,7 @@ package coelho
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"errors"
 )
 
 func Test_shouldReturnDefaultTweetingPaolo(t *testing.T) {
@@ -11,13 +12,13 @@ func Test_shouldReturnDefaultTweetingPaolo(t *testing.T) {
 }
 
 func Test_shouldCreateNewPaolo(t *testing.T) {
-	twitterClient := &FakeTwitterClient{}
+	twitterClient := &FakeTwitterClient{Msg:"test tweet"}
 	paolo := NewTweetingPaolo(twitterClient)
 	assert.NotNil(t, paolo)
 }
 
 func Test_shouldGetRandomTweet(t *testing.T) {
-	twitterClient := &FakeTwitterClient{}
+	twitterClient := &FakeTwitterClient{Msg:"test tweet"}
 	paolo := NewTweetingPaolo(twitterClient)
 
 	randomTweet := paolo.RandomTweet()
@@ -26,8 +27,20 @@ func Test_shouldGetRandomTweet(t *testing.T) {
 	assert.Equal(t, "test tweet", randomTweet.Sentence)
 }
 
-type FakeTwitterClient struct{}
+func Test_shouldBringDarknessOnNoTweet(t *testing.T) {
+	twitterClient := &FakeTwitterClient{Err: errors.New("No tweet for you")}
+	paolo := NewTweetingPaolo(twitterClient)
+
+	randomTweet := paolo.RandomTweet()
+
+	assert.Equal(t, "darkness", randomTweet.Sentence)
+}
+
+type FakeTwitterClient struct{
+	Msg string
+	Err error
+}
 
 func (fake *FakeTwitterClient) Tweets(screenName string) ([]string, error) {
-	return []string{"test tweet"}, nil
+	return []string{fake.Msg}, fake.Err
 }
